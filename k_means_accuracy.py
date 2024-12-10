d684e965660c6c646f66aa1436aeb0220153d1bf
NotsoNoob <161997589+NotsoNoob@users.noreply.github.com>
2024-12-09 21:10:06 -0600

    Create k_means_accuracy.py

---
+++ k_means_accuracy.py
@@ -0,0 +1,11 @@
+
+# running KNN algorithm for 3 to 50 nearest neighbours(odd numbers) and 
+# storing the accuracy values
+
+for i in range(1, 50, 2):
+    neigh = KNeighborsClassifier(n_neighbors = i)
+    neigh.fit(X_train, Y_train)
+    train_acc = np.mean(neigh.predict(X_train) == Y_train)
+    test_acc = np.mean(neigh.predict(X_test) == Y_test)
+    acc.append([train_acc, test_acc])
+
